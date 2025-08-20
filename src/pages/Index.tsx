f1d2610738b4c740b9aed01287610dfd019e70c9
Ckilic123 <kilicccem@gmail.com>
2025-08-20 15:23:53 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,12 @@
-
 import React from 'react';
-import Navigation from '@/components/Navigation';
 import HeroSection from '@/components/HeroSection';
 import ProjectSection from '@/components/ProjectSection';
 
-import { useScrollReveal } from '@/hooks/useScrollReveal';
-
-const Index = () => {
-  useScrollReveal();
-
+export default function IndexPage() {
   return (
-    <div className="min-h-screen page-scale">
-      <Navigation />
+    <div>
       <HeroSection />
-      <div className="mt-12">
-        <ProjectSection />
-      </div>
+      <ProjectSection />
     </div>
   );
-};
-
-export default Index;
+}
